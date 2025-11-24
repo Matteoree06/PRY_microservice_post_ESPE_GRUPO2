1f1f7b59c9582aa4a8b5b34130609890f2732d48
DajavYan <djyandun@espe.edu.ec>
2025-11-23 22:20:20 -0500

    ADD: Add functions in postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -11,7 +12,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Post::all(), 200);
     }
 
     /**
@@ -25,22 +26,47 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      */
+    
+    
+    // Crear un Post
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'title'     => 'required|string|max:255',
+            'content'   => 'required|string',
+            'user_id'   => 'required|integer',
+            'published' => 'boolean'
+        ]);
+
+        $post = Post::create($validated);
+
+        return response()->json([
+            'message' => 'Post creado correctamente',
+            'post' => $post
+        ], 201);
     }
 
     /**
      * Display the specified resource.
      */
+    
+    
+    // Obtener un Post por ID
     public function show(string $id)
     {
-        //
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json(['message' => 'Post no encontrado'], 404);
+        }
+
+        return response()->json($post, 200);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
+    // Editar un Post (no implementado)
     public function edit(string $id)
     {
         //
@@ -49,16 +75,47 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      */
+    
+    //Actualizar un Post
     public function update(Request $request, string $id)
     {
-        //
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json(['message' => 'Post no encontrado'], 404);
+        }
+
+        $validated = $request->validate([
+            'title'     => 'string|max:255',
+            'content'   => 'string',
+            'user_id'   => 'integer',
+            'published' => 'boolean'
+        ]);
+
+        $post->update($validated);
+
+        return response()->json([
+            'message' => 'Post actualizado correctamente',
+            'post' => $post
+        ], 200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
+    
+    // Eliminar un Post
     public function destroy(string $id)
     {
-        //
+        $post = Post::find($id);
+
+        if (!$post) {
+            return response()->json(['message' => 'Post no encontrado'], 404);
+        }
+
+        $post->delete(); 
+
+        return response()->json(['message' => 'Post eliminado'], 200);
+    
     }
 }
